1f4a83b96647cb9fc2719e9b6363ce90675f3006
Jeremy Williams <jeremy@uken.com>
2024-01-27 15:22:19 -0500

    Clean up gfx_pictures

--- src/gfx_pictures.c
+++ src/gfx_pictures.c
@@ -5,6 +5,7 @@
 #include "readfile.h"
 
 #define EGA_8x8_PLANE_SIZE 8
+#define EGA_BITS_PER_PIXEL 4
 #define UNMASKED_16x16_TILE_SIZE 128
 #define MASKED_16x16_TILE_SIZE 160
 
@@ -20,33 +21,15 @@ struct gfx_pictures {
   struct gfx_picture buffer[0];
 };
 
-void gfx_pictures_decode(struct gfx_pictures *pictures, uint16_t *picture_table,
-                         struct gfx_decoder *decoder, int n, int numplanes) {
-  for (int i = 0; i < n; i++) {
-    uint8_t *decoded_chunk = gfx_decoder_current_ptr(decoder);
-    pictures->buffer[i].chunk_id = gfx_decoder_current_cid(decoder);
-    int size = gfx_decoder_next_chunk(decoder);
-    pictures->buffer[i].width = picture_table[i * U16_SIZE] * 8;
-    pictures->buffer[i].height = picture_table[i * U16_SIZE + 1];
-    int plane_size = size / numplanes;
-    for (int j = 0; j < numplanes; j++) {
-      pictures->buffer[i].planes[j] = decoded_chunk + plane_size * j;
-    }
-  }
-}
-
-void gfx_pictures_deserialize_tiles(struct gfx_pictures *pictures,
-                                    uint8_t *raw_ega, int n, int size,
-                                    int numplanes, int chunk_id) {
-  for (int i = 0; i < n; i++) {
-    pictures->buffer[i].chunk_id = chunk_id;
-    pictures->buffer[i].width = size;
-    pictures->buffer[i].height = size;
-    int plane_size = size * size / BITS_PER_BYTE;
-    for (int j = 0; j < numplanes; j++) {
-      pictures->buffer[i].planes[j] = raw_ega;
-      raw_ega += plane_size;
-    }
+void gfx_picture_init(struct gfx_picture *picture, int width, int height,
+                      uint16_t chunk_id, int numplanes, uint8_t *ega) {
+  picture->width = width;
+  picture->height = height;
+  picture->chunk_id = chunk_id;
+  int plane_size =
+      width * height * numplanes * EGA_BITS_PER_PIXEL / BITS_PER_BYTE;
+  for (int i = 0; i < numplanes; i++) {
+    picture->planes[i] = ega + plane_size * i;
   }
 }
 
@@ -62,7 +45,14 @@ struct gfx_pictures *gfx_pictures_create_from_table(uint16_t *table, int n,
   pictures->n = n;
 
   // decode pictures
-  gfx_pictures_decode(pictures, table, decoder, n, numplanes);
+  for (int i = 0; i < n; i++) {
+    uint8_t *decoded_chunk = gfx_decoder_current_ptr(decoder);
+    gfx_decoder_next_chunk(decoder);
+    int chunk_id = gfx_decoder_current_cid(decoder);
+
+    gfx_picture_init(&pictures->buffer[i], table[i * 2], table[i * 2 + 1],
+                     chunk_id, numplanes, decoded_chunk);
+  }
 
   return pictures;
 }
@@ -85,8 +75,10 @@ gfx_pictures_create_from_8x8_tile_chunk(struct gfx_decoder *decoder,
   pictures->n = numunmasked8x8;
 
   // decode pictures
-  gfx_pictures_deserialize_tiles(pictures, raw_ega, numunmasked8x8, 8,
-                                 numplanes, chunk_id);
+  for (int i = 0; i < pictures->n; i++) {
+    gfx_picture_init(&pictures->buffer[i], 8, 8, chunk_id, numplanes, raw_ega);
+    raw_ega += EGA_8x8_PLANE_SIZE * numplanes;
+  }
 
   return pictures;
 }
@@ -106,15 +98,11 @@ struct gfx_pictures *gfx_pictures_create_from_tiles(struct gfx_decoder *decoder,
   pictures->n = tile_count;
 
   for (int i = 0; i < tile_count; i++) {
-    pictures->buffer[i].chunk_id = gfx_decoder_current_cid(decoder);
-    pictures->buffer[i].width = tile_size;
-    pictures->buffer[i].height = tile_size;
     uint8_t *raw_ega = gfx_decoder_current_ptr(decoder);
     gfx_decoder_next_unsized_chunk(decoder, UNMASKED_16x16_TILE_SIZE);
-    int plane_size = tile_size * tile_size / BITS_PER_BYTE;
-    for (int j = 0; j < numplanes; j++) {
-      pictures->buffer[i].planes[j] = raw_ega + plane_size * j;
-    }
+
+    gfx_picture_init(&pictures->buffer[i], tile_size, tile_size, 0, numplanes,
+                     raw_ega);
   }
 
   return pictures;
